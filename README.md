ebcf3c36f90b6275413f652c9afbbcfe5cd9e191
Anderson Juhasc <anderson.juhasc@gmail.com>
2019-05-07 14:17:16 -0300

    add seed and ajusts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BIP84
 
-Derives segwit + bech32 addresses from zprv/zpub and vprv/vpub in javascript
+Derives segwit + bech32 addresses from seed, zprv/zpub and vprv/vpub in javascript
 
 ## Installing
 
@@ -11,17 +11,37 @@ Run - `npm install bip84 --save`
 ```javascript
 var BIP84 = require('bip84')
 
-var vprv = new BIP84('vprv9KacKBSdiYsCfJSMa2tJekrhxxvJWhpoShz5SV8MJkt4av2wiqgMWKWddjAxtizdFYmg5o4ybHWkXWSSe2g4sKKbZwFxUdu6rWoXvoACsVy')
-var vpub = new BIP84('vpub5YZxigyXYvRVsnWpg4RK1toSWzknvAYeovugEsXxs6R3TiN6GNzc47q7UzcEiFkhwmzTcx83B61PEuXXkrTWPMbyjcZXBbe9BU8oKcrHb1g')
+var mnemonic = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
+var root = new BIP84(mnemonic)
 
-var zprv = new BIP84('zprvAd8nxfJY24ad35tHSynGRpdAPymTj8hQhYG75pjgpwz9mpZ3Sq2G2YN1VmfXsWdZeXPkpouQReKZnj7VsTC69nVnubL2U2gZeUwmGot9AaW')
-var zpub = new BIP84('zpub6r89NAqRrS8vFZxkZ1KGnxZtx1bx8bRG4mBhtD9JPHX8ectBzNLWaLgVM3owthf1JVAWqLgZdsUugsDDoHrq5RdRMAtiUTaEmyUaaAkasVu')
+console.log('mnemonic:', mnemonic)
+console.log('rootpriv:', root.getRootPrivate())
+console.log('rootpub:', root.getRootPublic())
+console.log('\n');
 
-console.log(vprv.getAddress(0))
-console.log(vpub.getAddress(0))
+var account0 = new BIP84(root.deriveAccount(0))
 
-console.log(zprv.getAddress(0))
-console.log(zpub.getAddress(0))
+console.log("Account 0, root = m/84'/0'/0'");
+console.log('account 0 xprv:', account0.getRootPrivate())
+console.log('account 0 xpub:', account0.getRootPublic())
+console.log('\n');
+
+console.log("Account 0, first receiving address = m/84'/0'/0'/0/0");
+console.log('prvkey:', account0.getPrivateKey(0))
+console.log('pubkey:', account0.getPublicKey(0))
+console.log('address:', account0.getAddress(0))
+console.log('\n');
+
+console.log("Account 0, first receiving address = m/84'/0'/0'/0/1");
+console.log('prvkey:', account0.getPrivateKey(1))
+console.log('pubkey:', account0.getPublicKey(1))
+console.log('address:', account0.getAddress(1))
+console.log('\n');
+
+console.log("Account 0, first change address = m/84'/0'/0'/1/0");
+console.log('prvkey:', account0.getPrivateKey(0, true))
+console.log('pubkey:', account0.getPublicKey(0, true))
+console.log('address:', account0.getAddress(0, true))
 ```
 
 ## Reference
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const bjs = require('bitcoinjs-lib')
     , b58 = require('bs58check')
+    , bip39 = require('bip39')
 
-function bip84(pub) {
+function bip84(pub, network) {
   // mainnet
   this.pub_types = [
     '04b2430c', // zprv
@@ -14,13 +15,17 @@ function bip84(pub) {
     '045f1cf6', // vpub
   ]
 
-  this.network = undefined
+  this.network = network && network === 'testnet' ? bjs.networks.testnet : bjs.networks.bitcoin
   this.pub = pub
   this.node = this.toNode()
 }
 
 // this function takes zpub/vpub and turns into xpub
 bip84.prototype.toNode = function() {
+  if (bip39.validateMnemonic(this.pub)) {
+    return bip39.mnemonicToSeedSync(this.pub)
+  }
+
   let payload = b58.decode(this.pub)
     , version = payload.slice(0, 4)
     , key = payload.slice(4)
@@ -49,13 +54,80 @@ bip84.prototype.toNode = function() {
   return b58.encode(buffer)
 }
 
+bip84.prototype.zprv = function (pub) {
+  let payload = b58.decode(pub)
+    , version = payload.slice(0, 4)
+    , key = payload.slice(4)
+
+  return b58.encode(Buffer.concat([Buffer.from('04b2430c','hex'), key]))
+}
+
+bip84.prototype.zpub = function (pub) {
+  let payload = b58.decode(pub)
+    , version = payload.slice(0, 4)
+    , key = payload.slice(4)
+
+  return b58.encode(Buffer.concat([Buffer.from('04b24746','hex'), key]))
+}
+
+bip84.prototype.getRootPrivate = function () {
+  let account = bip39.validateMnemonic(this.pub) ?
+        bjs.bip32.fromSeed(this.node).toBase58() :
+          bjs.bip32.fromBase58(this.node, this.network).toBase58()
+
+  return this.zprv(account)
+}
+
+bip84.prototype.getRootPublic = function () {
+  let account = bip39.validateMnemonic(this.pub) ?
+        bjs.bip32.fromSeed(this.node).neutered().toBase58() :
+          bjs.bip32.fromBase58(this.node, this.network).neutered().toBase58()
+
+  return this.zpub(account)
+}
+
+bip84.prototype.deriveAccount = function (index) {
+  let account = bip39.validateMnemonic(this.pub) ?
+        bjs.bip32.fromSeed(this.node).derivePath("m/84'/0'/" + index + "'").toBase58() :
+          bjs.bip32.fromBase58(this.node, this.network).derivePath("m/84'/0'/" + index + "'")
+
+  return this.zprv(account)
+}
+
+bip84.prototype.getPrivateKey = function (index, isChange) {
+  isChange = isChange !== true ? false : true
+
+  let change = isChange ? 1 : 0
+
+  let prvkey = bip39.validateMnemonic(this.pub) ?
+                 bjs.bip32.fromSeed(this.node).derivePath("m/84'/0'/0'").derive(change).derive(index) :
+                   bjs.bip32.fromBase58(this.node, this.network).derive(change).derive(index)
+
+  return prvkey.toWIF()
+}
+
+bip84.prototype.getPublicKey = function (index, isChange) {
+  isChange = isChange !== true ? false : true
+
+  let change = isChange ? 1 : 0
+    , pubkey = bip39.validateMnemonic(this.pub) ?
+                 bjs.bip32.fromSeed(this.node).derivePath("m/84'/0'/0'").derive(change).derive(index).publicKey :
+                   bjs.bip32.fromBase58(this.node, this.network).derive(change).derive(index).publicKey
+
+  return pubkey.toString('hex')
+}
+
 // this function takes an index, and turns the pubkey of that node into a Segwit bech32 address
 bip84.prototype.getAddress = function (index, isChange) {
   isChange = isChange !== true ? false : true
+
   let change = isChange ? 1 : 0
+    , pubkey = bip39.validateMnemonic(this.pub) ?
+                 bjs.bip32.fromSeed(this.node).derivePath("m/84'/0'/0'").derive(change).derive(index).publicKey :
+                   bjs.bip32.fromBase58(this.node, this.network).derive(change).derive(index).publicKey
 
   const payment = bjs.payments.p2wpkh({
-    pubkey: bjs.bip32.fromBase58(this.node, this.network).derive(change).derive(index).publicKey,
+    pubkey: pubkey,
     network: this.network
   })
 
