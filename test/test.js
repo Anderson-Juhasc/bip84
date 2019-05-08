8ab36b8637d5a45b4353914d0489b4cc74ea42a4
Anderson Juhasc <anderson.juhasc@gmail.com>
2019-05-08 15:13:54 -0300

    add test fromZpub on testnet

--- test/test.js
+++ test/test.js
@@ -22,7 +22,7 @@ console.log('Pubkey:', account0.getPublicKey(0))
 console.log('Address:', account0.getAddress(0))
 console.log('\n');
 
-console.log("Account 0, first receiving address = m/84'/0'/0'/0/1");
+console.log("Account 0, second receiving address = m/84'/0'/0'/0/1");
 console.log('Prvkey:', account0.getPrivateKey(1))
 console.log('Pubkey:', account0.getPublicKey(1))
 console.log('Address:', account0.getAddress(1))
@@ -34,8 +34,8 @@ console.log('Pubkey:', account0.getPublicKey(0, true))
 console.log('Address:', account0.getAddress(0, true))
 console.log('\n');
 
-var zpub = 'zpub6mxCxqRAQMCArMRMK7MmZwuisKmjHzuxCZtPgs4fYFZB86VRaTB6mdGCtgSHxeMjwyNLYZntdS7pRQdUqTdLuKr2apGhv5dRzNj1Br8uLQ6'
-var account1 = new BIP84.fromZPub(zpub)
+var zpub = 'vpub5Vm8JiyeMgCWT2SqgFkoJyaovNQH8RCF3wAUKCrFAfRdVujdYubBrYUGtggtabj71XxvUQuS5r9AgT4VhGvax9gXEpdi9XBg7jHnvm1WDii'
+var account1 = new BIP84.fromZPub(zpub, true)
 
 console.log("Account 1, root = m/84'/0'/0'");
 console.log('Account 1 xpub:', account1.getAccountPublic());
@@ -46,7 +46,7 @@ console.log('Pubkey:', account1.getPublicKey(0))
 console.log('Address:', account1.getAddress(0))
 console.log('\n');
 
-console.log("Account 1, first receiving address = m/84'/0'/0'/0/1");
+console.log("Account 1, second receiving address = m/84'/0'/0'/0/1");
 console.log('Pubkey:', account1.getPublicKey(1))
 console.log('Address:', account1.getAddress(1))
 console.log('\n');
@@ -54,4 +54,9 @@ console.log('\n');
 console.log("Account 1, first change address = m/84'/0'/0'/1/0");
 console.log('Pubkey:', account1.getPublicKey(0, true))
 console.log('Address:', account1.getAddress(0, true))
+console.log('\n');
+
+console.log("Account 1, second change address = m/84'/0'/0'/1/1");
+console.log('Pubkey:', account1.getPublicKey(1, true))
+console.log('Address:', account1.getAddress(1, true))
 console.log('\n');
